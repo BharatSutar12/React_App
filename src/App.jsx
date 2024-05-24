da033076c6d8ce6091ffcf681b7c4eb801bf3148
BharatSutar12 <sutar7492@gmil.com>
2024-05-24 20:45:09 +0530

    push all files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
 import Blog from './comopents/Blog';
+import './comopents/jsp.practice'
 
 
 
@@ -42,6 +43,7 @@ function App() {
           <Blog title={blog.title}
             description={blog.description}
             imgurl={blog.imgurl}
+
           />
         ))
       }
